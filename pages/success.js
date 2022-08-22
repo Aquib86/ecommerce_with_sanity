97e84c7f873362635c1252cd693bf90d0e5dc0d3
Aquib86 <mohdaquib1986@gmail.com>
2022-08-22 20:30:01 +0530

    first commit

---
+++ pages/success.js
@@ -0,0 +1,54 @@
+import React,{useState,useEffect} from 'react'
+import Link from 'next/link';
+import{BsBagCheckFill} from 'react-icons/bs';
+import { runFireworks } from '../lib/utils';
+
+import { useStateContext } from '../context/StateContext';
+
+const success = () => {
+    const {setCartItems,setTotalPrice,setTotalQuantities}=useStateContext();
+    
+    useEffect(()=>{
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+        runFireworks();
+    },[]);
+
+
+
+    
+  return (
+    <div className='success-wrapper'>
+        <div className='success'>
+            <p className='icon'>
+                <BsBagCheckFill/>
+
+            </p>
+            <h2>
+                Thank you for your order!
+            </h2>
+            <p className="e-mail-msg">Check your email inbox for the receipt.
+                
+            </p>
+            <p className="description">If you have andy questions, please email
+            <a className="email" href="mailto:order@example.com">order@example.com</a>
+            </p>
+            <Link href="/">
+                <button type="button" width="300px" className='btn'>
+                    Continue Shopping
+
+                </button>
+            </Link>
+
+            
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default success
